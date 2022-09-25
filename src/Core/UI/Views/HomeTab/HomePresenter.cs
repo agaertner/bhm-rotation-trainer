fda2bcef673548c90719b451afe2202f7c364aee
Andreas G <13819164+agaertner@users.noreply.github.com>
2022-09-25 02:28:41 +0200

    🎉add project

---
+++ src/Core/UI/Views/HomeTab/HomePresenter.cs
@@ -0,0 +1,11 @@
+﻿using Blish_HUD.Graphics.UI;
+
+namespace Nekres.RotationTrainer.Core.UI.Views.HomeTab
+{
+    internal class HomePresenter : Presenter<HomeView, HomeModel>
+    {
+        public HomePresenter(HomeView view, HomeModel model) : base(view, model)
+        {
+        }
+    }
+}
